d3d948db2cd790ae1d96ee125bf3e198ce906e84
ZachPrice <zach.price105@gmail.com>
2024-04-15 14:22:23 -0700

    new release cpp-meowpow

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -2,8 +2,8 @@ hunter_config(CURL VERSION ${HUNTER_CURL_VERSION} CMAKE_ARGS HTTP_ONLY=ON CMAKE_
 hunter_config(Boost VERSION 1.70.0-p0)
 
 hunter_config(ethash VERSION 1.0.0
-    URL https://github.com/Meowcoin-Foundation/cpp-meowpow/archive/refs/tags/1.1.0.tar.gz
-    SHA1 df182c5d1fae0b0b7f830ea5c62347522834ab0b
+    URL https://github.com/Meowcoin-Foundation/cpp-meowpow/archive/refs/tags/m1.0.0.tar.gz
+    SHA1 7d0a572f7b42d9cc621aa38ee27dc0220c4f40b2
 )
 
 hunter_config(OpenCL VERSION
