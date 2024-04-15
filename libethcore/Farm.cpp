15456834628cc5adbc3ce88e601116a7af8c37cf
ZachPrice <zach.price105@gmail.com>
2024-04-15 14:47:27 -0700

    another dag change setting

--- libethcore/Farm.cpp
+++ libethcore/Farm.cpp
@@ -201,7 +201,7 @@ void Farm::setWork(WorkPackage const& _newWp)
         m_currentEc.epochNumber = _newWp.epoch * 4;
         m_currentEc.lightNumItems = _ec.light_cache_num_items;
         m_currentEc.lightSize = ethash::get_light_cache_size(_ec.light_cache_num_items);
-        m_currentEc.dagNumItems = ethash::calculate_full_dataset_num_items(_newWp.epoch);
+        m_currentEc.dagNumItems = ethash::calculate_full_dataset_num_items(_newWp.epoch * 4);
         m_currentEc.dagSize = ethash::get_full_dataset_size(m_currentEc.dagNumItems);
         m_currentEc.lightCache = _ec.light_cache;
 
