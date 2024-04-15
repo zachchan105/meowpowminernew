e609043e8e7504400f3ce70e9a303d6452cabc7e
ZachPrice <zach.price105@gmail.com>
2024-04-15 14:01:13 -0700

    revert commits

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,7 +1,7 @@
 hunter_config(CURL VERSION ${HUNTER_CURL_VERSION} CMAKE_ARGS HTTP_ONLY=ON CMAKE_USE_OPENSSL=ON CMAKE_USE_LIBSSH2=OFF CURL_CA_PATH=none)
 hunter_config(Boost VERSION 1.70.0-p0)
 
-hunter_config(meowpow VERSION 1.0.0
+hunter_config(ethash VERSION 1.0.0
     URL https://github.com/Meowcoin-Foundation/cpp-meowpow/releases/tag/1.1.0
     SHA1 df182c5d1fae0b0b7f830ea5c62347522834ab0b
 )
